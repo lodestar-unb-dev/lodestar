ca6e612e6b537aa4a463f53271a7cd49b54a1091
Vini Fraga <vinifragam@gmail.com>
2021-05-28 18:47:22 -0300

    adds title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+        <title>LODESTAR UnB</title>
+
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet" />
         
