4e2cec5586343078599286f8cdeb50925c83078a
Vinícius Fraga <vinifragam@gmail.com>
2021-08-13 22:08:15 -0300

    fix: About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -12,70 +12,96 @@ export default function About() {
       name: 'Renato Borges',
       organization: 'ENE/UnB',
       imageUrl: '/teamMembers/renatoBorges.webp',
-      bio: 'Coordinator focused on Control Systems.',
+      role: 'Professor/Coordinator',
+      researchArea: 'Control Systems',
       lattesUrl: 'http://lattes.cnpq.br/9986885788660516'
     },
     {
       name: 'Daniel Café',
       organization: 'ENE/UnB',
       imageUrl: '/teamMembers/danielCafe.webp',
-      bio: 'Vice-coordinator focused on Embbeded Systems.',
+      role: 'Professor',
+      researchArea: 'Embbeded Systems',
       lattesUrl: 'http://lattes.cnpq.br/5467474463267175'
     },
     {
       name: 'João Paulo Leite',
       organization: 'ENE/UnB',
-      imageUrl: '/teamMembers/unknown.webp',
-      bio: 'Researcher focused on Telecommunication.',
+      imageUrl: '/teamMembers/avatar-male.svg',
+      role: 'Professor',
+      researchArea: 'Telecommunication',
       lattesUrl: 'http://lattes.cnpq.br/9310657002482326'
     },
     {
       name: 'João Gondim',
       organization: 'CIC/UnB',
       imageUrl: '/teamMembers/gondim.webp',
-      bio: 'Researcher focused on Cybersecurity.',
+      role: 'Professor',
+      researchArea: 'Cybersecurity',
       lattesUrl: 'http://lattes.cnpq.br/1755987387907910'
     },
     {
       name: 'Geovany Borges',
       organization: 'ENE/UnB',
       imageUrl: '/teamMembers/geovany.webp',
-      bio: 'Researcher focused on Robotics.',
+      role: 'Professor',
+      researchArea: 'Robotics',
       lattesUrl: 'http://lattes.cnpq.br/6103264336506171'
     },
     {
       name: 'Chantal Cappelletti',
-      organization: 'External',
-      imageUrl: '/teamMembers/unknown.webp',
-      bio: 'Researcher focused on Mission Analysis.',
+      organization: 'UoN/UK',
+      imageUrl: '/teamMembers/avatar-female.svg',
+      role: 'Professor',
+      researchArea: 'Mission Analysis',
       lattesUrl: 'http://lattes.cnpq.br/4120937958034706'
     },
     {
       name: 'Simone Battistini',
-      organization: 'External',
+      organization: 'SHU/UK',
       imageUrl: '/teamMembers/simoneBattistini.webp',
-      bio: 'Researcher focused on Control Systems.',
+      role: 'Professor',
+      researchArea: 'Control Systems',
       lattesUrl: 'http://lattes.cnpq.br/0151016853523965'
     },
     {
       name: 'Andréa Santos',
       organization: 'EPR/UnB',
       imageUrl: '/teamMembers/andreaSantos.webp',
-      bio: 'Researcher focused on Systems Engineering.',
+      role: 'Professor',
+      researchArea: 'Systems Engineering',
       lattesUrl: 'http://lattes.cnpq.br/4558493368126565'
     },
     {
       name: 'William Reis',
       organization: 'FGA/UnB',
       imageUrl: '/teamMembers/williamReis.webp',
-      bio: 'Researcher focused on Orbital Dynamic.',
+      role: 'Professor',
+      researchArea: 'Orbital Dynamic',
       lattesUrl: 'http://lattes.cnpq.br/7752228013890691'
     },
     {
       name: 'Leonardo Aguayo',
       organization: 'FGA/UnB',
       imageUrl: '/teamMembers/leonardoAguayo.webp',
-      bio: 'Researcher focused on Telecommunication.',
+      role: 'Professor',
+      researchArea: 'Telecommunication',
+      lattesUrl: 'http://lattes.cnpq.br/4815464320872808'
+    },
+    {
+      name: 'Marcelo Karam',
+      organization: 'CPD/UnB',
+      imageUrl: '/teamMembers/marceloKaram.webp',
+      role: 'Stafft',
+      researchArea: 'Data Security',
+      lattesUrl: 'http://lattes.cnpq.br/0017208558479044'
+    },
+    {
+      name: 'Rogério Sousa',
+      organization: 'CPD  /UnB',
+      imageUrl: '/teamMembers/rogerioSousa.webp',
+      role: 'Stafft',
+      researchArea: 'Data Security',
       lattesUrl: 'http://lattes.cnpq.br/4815464320872808'
     }
   ]
@@ -86,7 +112,7 @@ export default function About() {
         <TeamMembersContainer>
           <header>
             <h2>Our team members</h2>
-            <p>The project team is entirely composed of professors, servers and students from UnB, also counting on the participation of external collaborators.</p>
+            <p>The project team is composed of professors, stafft and students from UnB, also counting on the participation of external collaborators.</p>
           </header>
 
           <div>
