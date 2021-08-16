5c12262303384393321d165cda5728d9b9baf5cd
Vinícius Fraga <vinifragam@gmail.com>
2021-08-16 11:13:25 -0300

    adds google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,8 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components'
 
+import { GA_TRACKING_ID } from '../utils/gtag';
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
@@ -32,6 +34,24 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `,
+            }}
+          />
+
           <title>LODESTAR UnB</title>
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
