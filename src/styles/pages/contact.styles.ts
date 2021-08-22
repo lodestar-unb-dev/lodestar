0e2056e64711bdbfb4d17cfdddfb0683b73fd06a
Vinícius Fraga <vinifragam@gmail.com>
2021-08-22 13:15:59 -0300

    responsive contact page

--- src/styles/pages/contact.styles.ts
+++ src/styles/pages/contact.styles.ts
@@ -9,13 +9,14 @@ export const Container = styled.div`
   flex: 1;
   flex-direction: column;
 
-  padding: 80px 20px;
+  padding: 80px 0;
   color: ${props => props.theme.colors.blue};
 
   header {
     margin: 0 auto;
     max-width: 1120px;
     width: 100%;
+    padding: 0 20px;
     
     display: flex;
     flex-direction: column;
@@ -49,15 +50,58 @@ export const Container = styled.div`
         svg {
           color: ${props => props.theme.colors.blueDark};
           margin-right: 16px;
+          flex-shrink: 0;
         }
 
         span {
           max-width: 28.125rem;
+          text-align: justify;
         }
       }
 
       li + li {
         margin-top: 20px;
+      }
+    }
+  }
+
+  @media(max-width: 960px) {
+    height: auto;
+
+    section {
+      flex-direction: column;
+      gap: 40px;
+
+      aside, iframe {
+        width: 100%;
+        height: auto;
+      }
+    }
+  }
+
+  @media(max-width: 560px) {
+    padding: 40px 0;
+
+    header {
+      align-items: flex-start;
+
+      h2 {
+        font-size: 2.5rem;
+      }
+    }
+
+    section {
+      margin-top: 40px;
+      flex-direction: column;
+      gap: 40px;
+
+      ul li span {
+        max-width: unset;
+      }
+
+      aside, iframe {
+        width: 100%;
+        height: auto;
       }
     }
   }
