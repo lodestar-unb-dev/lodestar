0f09837b79368757e5aec958191390e287ca2974
Vini Fraga <vinifragam@gmail.com>
2021-09-09 10:49:48 -0300

    adds prismic data and preview to projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,7 @@
-import Link from 'next/link';
-import { useEffect } from 'react';
+import { GetStaticProps } from 'next';
+import { Fragment, useEffect } from 'react';
+import PrismicDOM from 'prismic-dom';
+
 import { NoScrollLink } from '../components/NoScrollLink';
 
 import {
@@ -10,8 +12,97 @@ import {
   HighAltitudeBallooningProject,
   Divider
 } from '../styles/pages/projects.styles';
+import { getPrismicClient } from '../services/prismic';
 
-export default function Projects() {
+interface ProjectsPrismicDocument {
+  title: string;
+  subtitle: {
+    type: string;
+    text: string;
+  }[];
+  space_missions_title: string;
+  space_missions_projects: {
+    project_title: string;
+    project_description: {
+      type: string;
+      text: string;
+    }[];
+    project_logo: {
+      dimensions: {
+        width: number;
+        height: number;
+      };
+      alt: string;
+      url: string;
+    }
+    project_url: string;
+  }[];
+  small_sat_simulator_title: string;
+  small_sat_simulator_projects: {
+    project_title: string;
+    project_description: {
+      type: string;
+      text: string;
+    }[];
+    project_logo: {
+      dimensions: {
+        width: number;
+        height: number;
+      };
+      alt: string;
+      url: string;
+    }
+    project_url: string;
+  }[];
+  high_altitude_ballooning_title: string;
+  high_altitude_ballooning_projects: {
+    project_title: string;
+    project_description: {
+      type: string;
+      text: string;
+    }[];
+    project_logo: {
+      dimensions: {
+        width: number;
+        height: number;
+      };
+      alt: string;
+      url: string;
+    }
+    project_url: string;
+  }[];
+}
+
+interface ProjectsProps {
+  projectsPrismicDocument: ProjectsPrismicDocument | null;
+}
+
+export default function Projects({ projectsPrismicDocument }: ProjectsProps) {
+  if (!projectsPrismicDocument) {
+    return (
+      <div
+        style={{
+          color: 'black',
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center' 
+        }}
+      >Erro no carregamento das informações.</div>
+    )
+  }
+
+  const {
+    title,
+    subtitle,
+    space_missions_title,
+    space_missions_projects,
+    small_sat_simulator_title,
+    small_sat_simulator_projects,
+    high_altitude_ballooning_title,
+    high_altitude_ballooning_projects
+  } = projectsPrismicDocument;
+  
   useEffect(() => {
     const path = window.location.hash
     if (path && path.includes("#")) {
@@ -32,263 +123,162 @@ export default function Projects() {
   return (
     <Container>
       <ProjectsHeader>
-        <h2>Our Projects</h2>
-        <p>
-          These are the projects that our team is working on. 
-          Space Missions, Small SAT Simulator and High-Altitude Ballooning 
-          are our main focus.
-        </p>
+        <h2>{title}</h2>
+        <div 
+          dangerouslySetInnerHTML={{
+            __html: PrismicDOM.RichText.asHtml(subtitle)
+          }}
+        />
       </ProjectsHeader>  
 
       <SpaceMissionProject id="space-missions">
-        <h3>Space Missions</h3>
+        <h3>{space_missions_title}</h3>
 
-        <article>
-          <img src="/alfacrux_logo.webp" alt="AlfaCrux logo" />
+        {space_missions_projects.map((space_mission, index) => (
+          <Fragment key={space_missions_title}>
+            <article>
+              <img src={space_mission.project_logo.url} alt="AlfaCrux logo" />
 
-          <div>
-            <h4>
-              AlfaCrux
-            </h4>
+              <div>
+                <h4>
+                  {space_mission.project_title}
+                </h4>
 
-            <p>
-              The AlfaCrux mission is an educational and technological in orbit 
-              demonstration of narrowband communication solutions. It is a 1U 
-              university CubeSat satellite developed by students and professors 
-              from the University of Brasilia, Brazil. It is the first space mission 
-              financed by the Government of the Federal District of Brazil 
-              through the Federal District Research Support Foundation (FAPDF), 
-              that also counts with the support of the Brazilian Space Agency (AEB).
-              Come and be part of it!
-            </p>
+                <div 
+                  dangerouslySetInnerHTML={{
+                    __html: PrismicDOM.RichText.asHtml(space_mission.project_description)
+                  }}
+                />
 
-            <NoScrollLink href="/projects/alfacrux" passHref>
-              <a>Check More</a>
-            </NoScrollLink>
-          </div>
-        </article>
+                {space_mission.project_url ? space_mission.project_url.includes('http') ? (
+                    <a
+                      href={space_mission.project_url}
+                      target="_blank" 
+                      rel="noopener noreferrer"
+                    >
+                      Check More
+                    </a>
+                ) : (
+                  <NoScrollLink href={space_mission.project_url} passHref>
+                    <a>Check More</a>
+                  </NoScrollLink>
+                ) : (
+                  <span>Comming soon</span>
+                )}
+              </div>
+            </article>
 
-        <Divider dividerColor="blue" />
-
-        <article>
-          <img src="/wormsail_logo.webp" alt="WormSail logo" />
-
-          <div>
-            <h4>
-              WormSail
-            </h4>
-
-            <p>
-              A 2U CubeSat, the WormSail is a university satellite developed 
-              by students and professors from the University of Brasilia (UnB), 
-              Brazil, and the University of Nottingham (UoN), UK. 
-              The main mission is educational: to provide a hands-on experience 
-              to the students in the complete process of developing and operating 
-              a space mission. It aims to demonstrate in orbit a flight software 
-              algorithm to perform packet routing, a set of observation subsystems 
-              to collect images and data from on-board sensors, and a new 
-              subsystem for attitude maneuvers including a 3-axes magnetorquer 
-              and a passive deorbiting mechanism based on Origami-like structures 
-              and atomic oxygen corrosion. Get in touch and help WormSail be a 
-              great success!
-            </p>
-
-            
-            <span>Comming soon</span>
-          </div>
-        </article>
-
-        <Divider dividerColor="blue" />
-
-        <article>
-          <img 
-            src="/glee_logo.webp" 
-            alt="Glee logo" 
-            style={{
-              borderRadius: 12
-            }}
-          />
-
-          <div>
-            <h4>
-              Glee
-            </h4>
-
-            <p>
-              The Great Lunar Expedition for Everyone, GLEE 2023, is a mission 
-              to the surface of the Moon that will conduct science and test 
-              technology with hundreds of 5-gram LunaSats built by students 
-              from all around the world. The LODESTAR is proud to be one of the 
-              organizations currently involved. Check more information on the 
-              GLEE website!
-            </p>
-
-            <a 
-              href="https://www.glee2023.org/" 
-              target="_blank" 
-              rel="noopener noreferrer"
-            >
-              Check More
-            </a>
-          </div>
-        </article>
-
-        <Divider dividerColor="blue" />
-
-        <article>
-          <img src="/glonass_logo.webp" alt="Glonass logo" />
-
-          <div>
-            <h4>
-              Glonass
-            </h4>
-
-            <p>
-              The LODESTAR Satellite Laser Ranging (SLR) station is a Russian system 
-              collocated with a GNSS receiver with the first ranging session performed 
-              in May 2014. The system consists of a monostatic telescope setup with a 
-              Gregorian refractor of 0.25 m aperture. The laser system consists of a 
-              Nd:YAG laser at 532 nm wavelength with a pulse energy of 2.5 mJ, a typical 
-              pulse width of 200 ps and a repetition rate of 300 Hz. The system is 
-              tracking all satellites up to GNSS satellites under daylight conditions. 
-              On August 2018 the NASA International Laser Ranging Service (ILRS) 
-              accepted the station as a contributing system to the ILRS network. 
-              Considering the impact of the SLR technique in the establishment of the 
-              Global Geodetic Reference Frame and the Global Geodetic Observing System, 
-              improvements at technical level are essential for consolidating the 
-              regional network, which includes the human resources capabilities. 
-              It is the first permanent SLR station in Brazil and the LODESTAR team 
-              is proud to be coordinating it in the last years with the recognition of 
-              its contribution for the LARGE-4 mission of the ILRS, as well as 
-              professional approach, high standard project management and significant 
-              contribution to the GNSS Glonass spacecraft laser tracking.
-            </p>
-
-            
-            <a 
-              href="https://ilrs.gsfc.nasa.gov/network/stations/active/BRAL_station_info.html" 
-              target="_blank" 
-              rel="noopener noreferrer"
-            >
-              Check More
-            </a>
-          </div>
-        </article>
+            {index < space_missions_projects.length -1 && (
+              <Divider dividerColor="blue" />
+            )}
+          </Fragment>
+        ))}
       </SpaceMissionProject>
 
       <SmallSATSimulatorProject id="small-sat-simulator">
-        <h3>Small SAT Simulator</h3>
+        <h3>{small_sat_simulator_title}</h3>
 
-        <article>
-          <img src="/logo.webp" alt="Simulator logo" />
+        {small_sat_simulator_projects.map((sss, index) => (
+          <Fragment key={small_sat_simulator_title}>
+            <article>
+              <img src={sss.project_logo.url} alt={sss.project_logo.alt} />
 
-          <div>
-            <h4>
-              Simulator
-            </h4>
+              <div>
+                <h4>
+                  {sss.project_title}
+                </h4>
 
-            <p>
-              Attitude simulators are a great resource for designing and testing 
-              attitude determination and control algorithms for spacecraft. 
-              They have been used for decades equipped with different sensors and 
-              actuators to reproduce the operational conditions of a satellite in 
-              space and to experimentally validate hardware and algorithms. 
-              The LODESTAR facility provides to the user a magnetic field simulator 
-              paired to an air-bearing platform that can simulate the attitude 
-              motion of a nanosatellite in orbit. The aim of this system is to 
-              provide a hardware-in-the-loop facility for attitude determination 
-              and control systems of nanosatellites for several applications, 
-              such as testing of algorithms, sensors, and actuators, as well as 
-              satellite functional tests. Come to validate your ideas and 
-              be part of this team!
-            </p>
+                <div 
+                  dangerouslySetInnerHTML={{
+                    __html: PrismicDOM.RichText.asHtml(sss.project_description)
+                  }}
+                />
 
-            <span>Comming soon</span>
-          </div>
-        </article>
+                {sss.project_url ? sss.project_url.includes('http') ? (
+                    <a
+                      href={sss.project_url}
+                      target="_blank" 
+                      rel="noopener noreferrer"
+                    >
+                      Check More
+                    </a>
+                ) : (
+                  <NoScrollLink href={sss.project_url} passHref>
+                    <a>Check More</a>
+                  </NoScrollLink>
+                ) : (
+                  <span>Comming soon</span>
+                )}
+              </div>
+            </article>
+
+            {index < small_sat_simulator_projects.length -1 && (
+              <Divider dividerColor="black10" />
+            )}
+          </Fragment>
+        ))}
       </SmallSATSimulatorProject>
 
       <HighAltitudeBallooningProject id="high-altitude-ballooning">
-        <h3>High-altitude Ballooning</h3>
+        <h3>{high_altitude_ballooning_title}</h3>
 
-        <article>
-          <img src="/laicansat_logo.webp" alt="LAICAnSAT logo" />
+        {high_altitude_ballooning_projects.map((hab, index) => (
+          <Fragment key={high_altitude_ballooning_title}>
+            <article>
+              <img src={hab.project_logo.url} alt={hab.project_logo.alt} />
 
-          <div>
-            <h4>
-              LAICAnSat
-            </h4>
+              <div>
+                <h4>
+                  {hab.project_title}
+                </h4>
 
-            <p>
-              The high-altitude scientific platform developed at LODESTAR, 
-              called LAICAnSat, is a project that started in 2013 as an 
-              initiative to stimulate the study of aerospace systems and to 
-              provide a low-cost platform for hands-on aerospace education. 
-              The first flight tests took place in 
-              2014 (LAICAnSat-1 and LAICAnSat-2) and allowed to test early 
-              hardware solutions. Other four launches occurred in 2017 
-              (LAICAnSat-3, LAICAnSat-4, LAICAnSat-5 and LAICAnSat-5.1). 
-              LAICAnSat-3 and LAICAnSat-4 were launched to validate a new 
-              mechanical structure fabricated in accordance with the CubeSat 
-              standard and using rapid prototype technologies based on 3D 
-              printer manufacturing, the new PC/104 standard PCB with the 
-              on-board computer and embedded sensors, as well as two 360º 
-              spherical cameras.
-            </p>
+                <div 
+                  dangerouslySetInnerHTML={{
+                    __html: PrismicDOM.RichText.asHtml(hab.project_description)
+                  }}
+                />
 
-            <a
-              href="https://www.youtube.com/channel/UCDbPHM1djZ8KOvkJh46NqRA"
-              target="_blank" 
-              rel="noopener noreferrer"
-            >
-              Check More
-            </a>
-          </div>
-        </article>
+                {hab.project_url ? hab.project_url.includes('http') ? (
+                    <a
+                      href={hab.project_url}
+                      target="_blank" 
+                      rel="noopener noreferrer"
+                    >
+                      Check More
+                    </a>
+                ) : (
+                  <NoScrollLink href={hab.project_url} passHref>
+                    <a>Check More</a>
+                  </NoScrollLink>
+                ) : (
+                  <span>Comming soon</span>
+                )}
+              </div>
+            </article>
 
-        <Divider dividerColor="greenLight" />
-
-        <article>
-          <img 
-            src="/kuaray_logo.webp" 
-            alt="Kuaray logo"
-            style={{
-              borderRadius: 12
-            }} 
-          />
-
-          <div>
-            <h4>
-              Kuaray
-            </h4>
-
-            <p>
-              The main mission of the LAICAnSat platform took place at Idaho, 
-              USA, in 2017. The goal was to record the total solar eclipse 
-              from the stratosphere using a 360º degree camera, reproducing 
-              the flight experience up to the stratosphere during the total 
-              solar eclipse of August 2017 in North America, as well as to 
-              provide a vertical meteorological mapping of the whole flight path. 
-              It was a new achievement in the world, to the best of our 
-              knowledge never seen before! The LODESTAR team, and its partners 
-              Mutum Ham Radio Expedition team and the 
-              Brazilian Astronomy Club, DF, Brazil, are proud for having been a 
-              member of the NASA Space Grant Eclipse Ballooning Project. 
-              The LAICAnSat-5 (CubeSat 3U standard) and 
-              LAICAnSat-5.1 (Cube-Sat 1U standard) launch and the mission 
-              were a great success! Check this adventure and come to be a partner! 
-            </p>
-
-            <a
-              href="https://www.facebook.com/projetokuaray"
-              target="_blank" 
-              rel="noopener noreferrer"
-            >
-              Check More
-            </a>
-          </div>
-        </article>
+            {index < high_altitude_ballooning_projects.length -1 && (
+              <Divider dividerColor="greenLight" />
+            )}
+          </Fragment>
+        ))}
       </HighAltitudeBallooningProject>
     </Container>
   )
+}
+
+export const getStaticProps: GetStaticProps<ProjectsProps> = async ({
+  previewData
+}) => {
+  const correctlyTypedPreviewData = previewData as { ref: string } | null;
+  
+  const prismic = getPrismicClient();
+  const projectsResponse = await prismic.getSingle('projects', {
+    ref: correctlyTypedPreviewData?.ref ? correctlyTypedPreviewData.ref : ''
+  });
+
+  return {
+    props: {
+      projectsPrismicDocument: projectsResponse?.data ?? null,
+    }
+  }
 }
