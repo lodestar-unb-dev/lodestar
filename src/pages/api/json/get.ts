67f5b804fb543b27e6503279724302e8453548fd
Vini Fraga <vinifragam@gmail.com>
2022-08-16 23:38:11 -0300

    chore: update json files

--- src/pages/api/json/get.ts
+++ src/pages/api/json/get.ts
@@ -1,47 +1,37 @@
 import { NextApiResponse, NextApiRequest } from 'next'
 
-type SelectedFilesData = {
-  name: string;
-  calibrated_value: number;
-  raw_value: number;
-  within_limits: 'Yes' | 'No';
-  utc: string;
-  sat_utc: string;
-  gs_utc: string;
-}
+import P_OBC_TELEM_MAG_X from '../../../mocks/p_obc_telem_mag_x.json';
+import P_OBC_TELEM_MAG_Y from '../../../mocks/p_obc_telem_mag_y.json';
+import P_OBC_TELEM_MAG_Z from '../../../mocks/p_obc_telem_mag_z.json';
+import P_OBC_TELEM_GYRO_X from '../../../mocks/p_obc_telem_gyro_x.json';
+import P_OBC_TELEM_GYRO_Y from '../../../mocks/p_obc_telem_gyro_y.json';
+import P_OBC_TELEM_GYRO_Z from '../../../mocks/p_obc_telem_gyro_z.json';
+import P_OM_TELEM_SW_STATE from '../../../mocks/p_om_telem_sw_state.json';
 
-import P_OBC_TELEM_MAG_X from '../../../mocks/P_OBC_TELEM_MAG_X.json';
-import P_OBC_TELEM_MAG_Y from '../../../mocks/P_OBC_TELEM_MAG_Y.json';
-import P_OBC_TELEM_MAG_Z from '../../../mocks/P_OBC_TELEM_MAG_Z.json';
-import P_OBC_TELEM_GYRO_X from '../../../mocks/P_OBC_TELEM_GYRO_X.json';
-import P_OBC_TELEM_GYRO_Y from '../../../mocks/P_OBC_TELEM_GYRO_Y.json';
-import P_OBC_TELEM_GYRO_Z from '../../../mocks/P_OBC_TELEM_GYRO_Z.json';
-import P_OM_TELEM_SW_STATE from '../../../mocks/P_OM_TELEM_SW_STATE.json';
+import P_EP_TELEM_VBATT from '../../../mocks/p_ep_telem_vbatt.json';
+import P_EP_TELEM_CURIN_0 from '../../../mocks/p_ep_telem_curin_0.json';
+import P_EP_TELEM_CURIN_1 from '../../../mocks/p_ep_telem_curin_1.json';
+import P_EP_TELEM_CURIN_2 from '../../../mocks/p_ep_telem_curin_2.json';
+import P_EP_TELEM_CURSUN from '../../../mocks/p_ep_telem_cursun.json';
+import P_EP_TELEM_CURSYS from '../../../mocks/p_ep_telem_cursys.json';
+import P_EP_TELEM_VBOOST_0 from '../../../mocks/p_ep_telem_vboost_0.json';
+import P_EP_TELEM_VBOOST_1 from '../../../mocks/p_ep_telem_vboost_1.json';
+import P_EP_TELEM_VBOOST_2 from '../../../mocks/p_ep_telem_vboost_2.json';
+import P_EP_TELEM_CUROUT_0 from '../../../mocks/p_ep_telem_curout_0.json';
+import P_EP_TELEM_CUROUT_3 from '../../../mocks/p_ep_telem_curout_3.json';
+import P_EP_TELEM_CUROUT_5 from '../../../mocks/p_ep_telem_curout_5.json';
 
-import P_EP_TELEM_VBATT from '../../../mocks/P_EP_TELEM_VBATT.json';
-import P_EP_TELEM_CURIN_0 from '../../../mocks/P_EP_TELEM_CURIN_0.json';
-import P_EP_TELEM_CURIN_1 from '../../../mocks/P_EP_TELEM_CURIN_1.json';
-import P_EP_TELEM_CURIN_2 from '../../../mocks/P_EP_TELEM_CURIN_2.json';
-import P_EP_TELEM_CURSUN from '../../../mocks/P_EP_TELEM_CURSUN.json';
-import P_EP_TELEM_CURSYS from '../../../mocks/P_EP_TELEM_CURSYS.json';
-import P_EP_TELEM_VBOOST_0 from '../../../mocks/P_EP_TELEM_VBOOST_0.json';
-import P_EP_TELEM_VBOOST_1 from '../../../mocks/P_EP_TELEM_VBOOST_1.json';
-import P_EP_TELEM_VBOOST_2 from '../../../mocks/P_EP_TELEM_VBOOST_2.json';
-import P_EP_TELEM_CUROUT_0 from '../../../mocks/P_EP_TELEM_CUROUT_0.json';
-import P_EP_TELEM_CUROUT_3 from '../../../mocks/P_EP_TELEM_CUROUT_3.json';
-import P_EP_TELEM_CUROUT_5 from '../../../mocks/P_EP_TELEM_CUROUT_5.json';
-
-import P_TT_TELEM_TEMP_BRD from '../../../mocks/P_TT_TELEM_TEMP_BRD.json';
-import P_OBC_TELEM_TEMP_MCU from '../../../mocks/P_OBC_TELEM_TEMP_MCU.json';
-import P_EP_TELEM_TEMP_3 from '../../../mocks/P_EP_TELEM_TEMP_3.json';
-import P_EP_TELEM_TEMP_4 from '../../../mocks/P_EP_TELEM_TEMP_4.json';
-import P_EP_TELEM_TEMP_5 from '../../../mocks/P_EP_TELEM_TEMP_5.json';
-import P_SP_TELEM_TEMP_PX from '../../../mocks/P_SP_TELEM_TEMP_PX.json';
-import P_SP_TELEM_TEMP_NX from '../../../mocks/P_SP_TELEM_TEMP_NX.json';
-import P_SP_TELEM_TEMP_PY from '../../../mocks/P_SP_TELEM_TEMP_PY.json';
-import P_SP_TELEM_TEMP_NY from '../../../mocks/P_SP_TELEM_TEMP_NY.json';
-import P_SP_TELEM_TEMP_PZ from '../../../mocks/P_SP_TELEM_TEMP_PZ.json';
-import P_SP_TELEM_TEMP_NZ from '../../../mocks/P_SP_TELEM_TEMP_NZ.json';
+import P_TT_TELEM_TEMP_BRD from '../../../mocks/p_tt_telem_temp_brd.json';
+import P_OBC_TELEM_TEMP_MCU from '../../../mocks/p_obc_telem_temp_mcu.json';
+import P_EP_TELEM_TEMP_3 from '../../../mocks/p_ep_telem_temp_3.json';
+import P_EP_TELEM_TEMP_4 from '../../../mocks/p_ep_telem_temp_4.json';
+import P_EP_TELEM_TEMP_5 from '../../../mocks/p_ep_telem_temp_5.json';
+import P_SP_TELEM_TEMP_PX from '../../../mocks/p_sp_telem_temp_px.json';
+import P_SP_TELEM_TEMP_NX from '../../../mocks/p_sp_telem_temp_nx.json';
+import P_SP_TELEM_TEMP_PY from '../../../mocks/p_sp_telem_temp_py.json';
+import P_SP_TELEM_TEMP_NY from '../../../mocks/p_sp_telem_temp_ny.json';
+import P_SP_TELEM_TEMP_PZ from '../../../mocks/p_sp_telem_temp_pz.json';
+import P_SP_TELEM_TEMP_NZ from '../../../mocks/p_sp_telem_temp_nz.json';
 import { isAfter, parseISO } from 'date-fns';
 
 const jsonFiles = {
@@ -77,39 +67,6 @@ const jsonFiles = {
   'P_SP_TELEM_TEMP_NZ': P_SP_TELEM_TEMP_NZ,
 }
 
-const APIUnits = {
-  'P_OBC_TELEM_MAG_X': 'G',
-  'P_OBC_TELEM_MAG_Y': 'G',
-  'P_OBC_TELEM_MAG_Z': 'G',
-  'P_OBC_TELEM_GYRO_X': 'deg\/s',
-  'P_OBC_TELEM_GYRO_Y': 'deg\/s',
-  'P_OBC_TELEM_GYRO_Z': 'deg\/s',
-  'P_OM_TELEM_SW_STATE' : '',
-  'P_EP_TELEM_VBATT': 'mV',
-  'P_EP_TELEM_CURIN_0': 'mA',
-  'P_EP_TELEM_CURIN_1': 'mA',
-  'P_EP_TELEM_CURIN_2': 'mA',
-  'P_EP_TELEM_CURSUN': 'mA',
-  'P_EP_TELEM_CURSYS': 'mA',
-  'P_EP_TELEM_VBOOST_0': 'mV',
-  'P_EP_TELEM_VBOOST_1': 'mV',
-  'P_EP_TELEM_VBOOST_2': 'mV',
-  'P_EP_TELEM_CUROUT_0': 'mA',
-  'P_EP_TELEM_CUROUT_3': 'mA',
-  'P_EP_TELEM_CUROUT_5': 'mA',
-  'P_TT_TELEM_TEMP_BRD': 'deg',
-  'P_OBC_TELEM_TEMP_MCU': 'deg',
-  'P_EP_TELEM_TEMP_3': 'deg',
-  'P_EP_TELEM_TEMP_4': 'deg',
-  'P_EP_TELEM_TEMP_5': 'deg',
-  'P_SP_TELEM_TEMP_PX': 'deg',
-  'P_SP_TELEM_TEMP_NX': 'deg',
-  'P_SP_TELEM_TEMP_PY': 'deg',
-  'P_SP_TELEM_TEMP_NY': 'deg',
-  'P_SP_TELEM_TEMP_PZ': 'deg',
-  'P_SP_TELEM_TEMP_NZ': 'deg'
-}
-
 // info
 // api/get
 // param_name=P_SP_TELEM_TEMP_NX
@@ -124,31 +81,17 @@ export default function handler(
 ) {
   const { param_name, start_time } = request.query;
 
-  const selectedFile = jsonFiles[param_name as string] as SelectedFilesData[];
-
+  const selectedFile = jsonFiles[param_name as string];
+  
   if (!start_time) {
-    const result = selectedFile.map(item => ({
-      param_name: item.name,
-      calibrated_value: item.calibrated_value,
-      calibrated_units: APIUnits[item.name as string],
-      raw_value: item.raw_value,
-      timestamp_sat_utc_corrected: item.utc,
-      timestamp_gs_utc: item.gs_utc
-    }))
+    const result = selectedFile;
     return res.status(200).json(result);
   }
 
-  const index = selectedFile.findIndex(item => isAfter(parseISO(item.utc), parseISO(start_time as string)))
+  const index = selectedFile.findIndex(item => isAfter(parseISO(item.timestamp_sat_utc_corrected), parseISO(start_time as string)))
 
   if (index > 0) {
-    const result = selectedFile.slice(index).map(item => ({
-      param_name: item.name,
-      calibrated_value: item.calibrated_value,
-      calibrated_units: APIUnits[item.name as string],
-      raw_value: item.raw_value,
-      timestamp_sat_utc_corrected: item.utc,
-      timestamp_gs_utc: item.gs_utc
-    }))
+    const result = selectedFile.slice(index);
     return res.status(200).json(result);
   }
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'https://lodestar.aerospace.unb.br/api/json'
+  baseURL: 'http://localhost:3000/api/json'
 })
