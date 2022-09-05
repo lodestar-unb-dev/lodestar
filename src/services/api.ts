fca9a838bf0100c17fd436bd52432f821cf226b7
Vinícius Fraga <vinifragam@gmail.com>
2022-09-05 17:23:46 -0300

    change api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const { NEXT_PUBLIC_SITE_URL } = process.env;
+const { NEXT_PUBLIC_PROD_URL } = process.env;
 
 export const api = axios.create({
-  baseURL: `${NEXT_PUBLIC_SITE_URL}/api/json`
+  baseURL: `${NEXT_PUBLIC_PROD_URL}`
 })
