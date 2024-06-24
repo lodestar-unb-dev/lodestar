668870f38a1dd3320e344f23c9ace9a6af07a441
Vini Fraga <vinifragam@gmail.com>
2024-06-24 11:33:16 -0300

    chore: improve h2 align, partners and carousel size

--- src/app/(perception-header)/projects/perception/sections/partners.tsx
+++ src/app/(perception-header)/projects/perception/sections/partners.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image'
 
 import logo from '../assets/icons/logo-alt.svg'
-import unbLogo from '../assets/images/unb.avif'
+import unbLogo from '../assets/images/unb.jpg'
 import fapLogo from '../assets/images/fap.avif'
-import finatecLogo from '../assets/images/finatec.svg'
 import aebLogo from '../assets/images/aeb.avif'
 import anatelLogo from '../assets/images/anatel.avif'
 
@@ -15,13 +14,17 @@ export function Partners() {
       <div className="flex max-w-5xl flex-wrap justify-center gap-5 text-unbBlue-400">
         <div className="flex flex-col items-center gap-2">
           <p className="font-medium uppercase">Realization</p>
-          <Image className="w-40 mix-blend-multiply" src={unbLogo} alt="unb" />
+          <Image
+            className="h-20 w-auto mix-blend-multiply"
+            src={unbLogo}
+            alt="unb"
+          />
         </div>
 
         <div className="flex flex-col items-center gap-2">
           <p className="font-medium uppercase">Funder</p>
           <Image
-            className="w-40 mix-blend-multiply"
+            className="h-20 w-auto mix-blend-multiply"
             src={fapLogo}
             alt="fapdf"
           />
@@ -30,8 +33,8 @@ export function Partners() {
         <div className="flex flex-col items-center gap-2">
           <p className="font-medium uppercase">Partner and technical support</p>
           <div className="flex items-center gap-10">
-            <Image className="w-40" src={aebLogo} alt="aeb" />
-            <Image className="w-20" src={anatelLogo} alt="anatel" />
+            <Image className="h-20 w-auto" src={aebLogo} alt="aeb" />
+            <Image className="h-20 w-auto" src={anatelLogo} alt="anatel" />
           </div>
         </div>
       </div>
