9ea8547a6fe0bdc8be095f059f4fe7290bc52d56
Vinícius Fraga <vinifragam@gmail.com>
2024-07-10 18:03:19 -0300

    chore: open links on another tab and add section header to partners

--- src/app/(perception-header)/projects/perception/sections/partners.tsx
+++ src/app/(perception-header)/projects/perception/sections/partners.tsx
@@ -13,12 +13,22 @@ import alenLogo from '../assets/images/alen.png'
 export function Partners() {
   return (
     <section className="flex flex-col items-center gap-10 py-10">
+      <div className="h-5 w-full bg-[#EE761B]" />
+
+      <h2 className="text-left text-xl font-bold text-[#1A3E76] md:text-3xl">
+        ACKNOWLEDGMENTS
+      </h2>
+
       <Image src={logo} alt="perception" className="w-[216px]" />
 
       <div className="flex max-w-5xl flex-wrap justify-center gap-5 text-unbBlue-400">
         <div className="flex flex-col items-center gap-2">
           <p className="font-medium uppercase">Realization</p>
-          <a href="https://www.unb.br/">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.unb.br/"
+          >
             <Image
               className="h-20 w-auto mix-blend-multiply"
               src={unbLogo}
@@ -29,7 +39,11 @@ export function Partners() {
 
         <div className="flex flex-col items-center gap-2">
           <p className="font-medium uppercase">Funder</p>
-          <a href="https://www.fap.df.gov.br/">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.fap.df.gov.br/"
+          >
             <Image
               className="h-20 w-auto mix-blend-multiply"
               src={fapLogo}
@@ -41,30 +55,54 @@ export function Partners() {
         <div className="flex flex-col items-center gap-2">
           <p className="font-medium uppercase">Partner and technical support</p>
           <div className="flex flex-wrap items-center justify-center gap-10">
-            <a href="https://lba.inpa.gov.br/">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://lba.inpa.gov.br/"
+            >
               <Image className="h-32 w-auto" src={lbaLogo} alt="LBA" />
             </a>
-            <a href="http://www.uea.edu.br/">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="http://www.uea.edu.br/"
+            >
               <Image
                 className="h-20 w-auto mix-blend-multiply"
                 src={ueaLogo}
                 alt="UEA"
               />
             </a>
-            <a href="https://alen.space/">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://alen.space/"
+            >
               <Image className="h-20 w-auto" src={alenLogo} alt="alen" />
             </a>
-            <a href="https://www.uvigo.gal/es">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.uvigo.gal/es"
+            >
               <Image
                 className="h-32 w-auto"
                 src={vigoLogo}
                 alt="UniversidadeVigo"
               />
             </a>
-            <a href="https://www.gov.br/aeb/pt-br">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.gov.br/aeb/pt-br"
+            >
               <Image className="h-20 w-auto" src={aebLogo} alt="aeb" />
             </a>
-            <a href="https://www.gov.br/anatel/pt-br">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.gov.br/anatel/pt-br"
+            >
               <Image className="h-20 w-auto" src={anatelLogo} alt="anatel" />
             </a>
           </div>
